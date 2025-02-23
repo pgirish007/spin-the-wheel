4e5bb99bc519897b288fdb5e55299cde9090c599
Girish Pandit <pandit.girish@gmail.com>
2025-02-23 18:46:28 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./index.css";
+import { React, useState } from "react";
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from "./components/Home";
+
+function App() {
+ return ( 
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/home" element={<Home/>}/>
+  </Routes>
+  </BrowserRouter>
+
+ )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// filepath: /Users/girishpandit/Development/ChatBOT/spin-the-wheel/webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    library: 'WheelApp',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
